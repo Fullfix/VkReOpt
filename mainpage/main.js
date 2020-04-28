52d3638e7b2a0a6b85da374413c0b5a8f02a8fd6
Nikita <nbreskanu73@gmail.com>
2020-04-28 19:39:26 +0400

    Revert "Revert "Revert "Arrows from hell that creates million mistakes created"""
    This reverts commit 2e142ae3e82a87362da6047de60e50a8d08f600e.

--- mainpage/main.js
+++ mainpage/main.js
@@ -1,4 +1,3 @@
-let GridElSize = document.getElementById("gridElSize");
 const root = document.documentElement;
 let maxid = -1;
 root.style.setProperty('--typeGrid', "auto-fill");
@@ -12,23 +11,21 @@ const deleteMessage = function() {
     grid.remove(this.parentElement.parentElement)
     this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
     chrome.storage.local.get("messages", (obj) => {
-        let newMessages = obj.messages.filter(msg => msg[4] != id);
+        let newMessages = obj.messages.filter(msg => msg[3] != id);
         chrome.storage.local.set({messages: newMessages});
     })
 }
-function matrixToArray(str) {
-    return str.match(/(-?[0-9\.]+)/g);
-}
+
 const changeMessageOrder = (messages) => {
     let ids = Array.from(messages).map(elem => elem.id.split("message")[1]);
     chrome.storage.local.get("messages", (obj) => {
         let newMessages = [];
         for (let id of ids) {
-            let message = obj.messages.find(msg => msg[4] == id);
+            let message = obj.messages.find(msg => msg[3] == id);
             newMessages.push(message);
         }
         chrome.storage.local.set({messages: newMessages});
-    })  
+    })
 }
 
 let grid = new Muuri('.grid', {dragEnabled: true, dragPlaceholder: {
@@ -38,20 +35,19 @@ let grid = new Muuri('.grid', {dragEnabled: true, dragPlaceholder: {
     }, dragSortPredicate:{
         threshold: 30
     }});
-/*grid.on('dragReleaseEnd', (item) => {
+grid.on('dragReleaseEnd', (item) => {
     changeMessageOrder(grid.getItems().map(elem => {
         return elem.getElement().getElementsByClassName("item-content")[0];
     }));
-})*/
-let maxh = -1;
+})
 const loadScreen = function() {
     document.getElementById("grid").innerHTML = ""
     chrome.storage.local.get("messages", (obj) => {
         for (let message of obj.messages) {
-            maxh = -1;
+            let maxh = -1;
             root.style.setProperty('--height', "auto");
             let messageDiv = document.createElement("div");
-            messageDiv.id = "message" + message[4];
+            messageDiv.id = "message" + message[3];
             messageDiv.className = "item-content";
             let textDiv = document.createElement("div");
             textDiv.innerHTML = message[0];
@@ -77,137 +73,18 @@ const loadScreen = function() {
             messageCont.className = "item"
             messageCont.appendChild(messageDiv);
             grid.add(messageCont);
-            console.log(message[3])
-            console.log(message[4])
-            if (typeof message[3] != 'number')
-            messageCont.id = message[4];
-            else messageCont.id = message[3];
-            messageCont.setAttribute('linked', false);
+            messageCont.id = message[3];
             if (maxid < messageCont.id) maxid = messageCont.id;
             for (let i = 1; i <= maxid+1; i++){
                 if (document.getElementById(i) != null && maxh < document.getElementById(i).clientHeight) maxh = document.getElementById(i).clientHeight;
             }
             root.style.setProperty('--height', maxh-10 + "px");
-            for (let i in message[3]){
-                let imgImage = document.createElement('img');
-                imgImage.src = message[3][i];
-                console.log(message[3][i]);
-                let mul = imgImage.naturalWidth / +GridElSize.value;
-                console.log(imgImage.naturalWidth);
-                console.log(+GridElSize.value);
-                console.log(mul);
-                imgImage.width = GridElSize.value;
-                imgImage.height = imgImage.naturalHeight / mul;
-                imgImage.height = imgImage.height - imgImage.height % (maxh + 6);
-                let imgDiv = document.createElement('div');
-                imgDiv.appendChild(imgImage);
-                imgDiv.className = "item-content";
-                let imgCont = document.createElement('div');
-                imgCont.appendChild(imgDiv);
-                imgCont.className = "item";
-                imgCont.id = 'img' + message[4] + i;
-                grid.add(imgCont);
-                imgCont.style.width = imgImage.width + 'px';
-                imgCont.style.height = imgImage.height + 'px';
-                let sv = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-                let rope = document.createElementNS("http://www.w3.org/2000/svg", "line");
-                sv.setAttribute('class', "lineSV")
-                let fe = messageCont;
-                let se = imgCont;
-                let coors1 = matrixToArray(fe.style.transform);
-                let coors2 = matrixToArray(se.style.transform);
-                sv.appendChild(rope);
-                rope.id = "line" + message[4] + i;
-                rope.setAttribute('x1', (+coors2[0] + +GridElSize.value / 2));
-                rope.setAttribute('from', messageCont.id);
-                rope.setAttribute('to', imgCont.id);
-                rope.setAttribute('y1', (+coors2[1] + maxh / 2));
-                rope.setAttribute('x2', (+coors1[0] + +GridElSize.value / 2));
-                rope.setAttribute('y2', (+coors1[1] + maxh / 2));
-                rope.setAttribute('stroke', "red");
-                messageCont.setAttribute('linked', true);
-                imgCont.setAttribute('linked', true);
-                document.querySelector('.wrapper').appendChild(sv);
-
-            }
             grid.refreshItems().layout();
         }
     })
 
 }
-grid.on('dragMove', function (item, event) {
-    if (item._element.getAttribute('linked') == 'true'){
-        let ropes = document.querySelectorAll('line');
-        let el = item.getElement();
-        for (let i in ropes){
-            console.log(ropes[i])
-            try {
-                if (ropes[i].getAttribute('from') == el.id){
-                    let coord = matrixToArray(el.style.transform);
-                    ropes[i].setAttribute('x2', +coord[0] + +GridElSize.value / 2)
-                    ropes[i].setAttribute('y2', +coord[1] + maxh / 2)
-                }
-                else if (ropes[i].getAttribute('to') == el.id){
-                    let coord = matrixToArray(el.style.transform);
-                    ropes[i].setAttribute('x1', +coord[0] + +GridElSize.value / 2)
-                    ropes[i].setAttribute('y1', +coord[1] + maxh / 2)
-                }
-            } catch(e){
-                console.log('Пофиг...')
-            }
 
-        }
-    }
-});
-grid.on('dragEnd', function(item, event){
-    if (item._element.getAttribute('linked') == 'true'){
-        let interval = setInterval(()=>{
-            let ropes = document.querySelectorAll('line');
-            let el = item.getElement();
-            for (let i in ropes){
-                console.log(ropes[i])
-                try {
-                    if (ropes[i].getAttribute('from') == el.id){
-                        let coord = matrixToArray(el.style.transform);
-                        ropes[i].setAttribute('x2', +coord[0] + +GridElSize.value / 2)
-                        ropes[i].setAttribute('y2', +coord[1] + maxh / 2)
-                    }
-                    else if (ropes[i].getAttribute('to') == el.id){
-                        let coord = matrixToArray(el.style.transform);
-                        ropes[i].setAttribute('x1', +coord[0] + +GridElSize.value / 2)
-                        ropes[i].setAttribute('y1', +coord[1] + maxh / 2)
-                    }
-                    clearInterval(interval)
-                } catch(e){
-                    console.log('Пофиг...')
-                }
-
-            }}, 100);
-    }
-})
-grid.on('move', function(item, event){
-    console.log('dd')
-    let interval2 = setInterval(()=>{
-        let ropes = document.querySelectorAll('line');
-        for (let j in ropes){
-            try {
-            let fs = document.getElementById(ropes[j].getAttribute('from'));
-            } catch(e){
-                console.log('СверхПофиг...')
-                clearInterval(interval2)
-            }
-            let fs = document.getElementById(ropes[j].getAttribute('from'));
-            let sc = document.getElementById(ropes[j].getAttribute('to'));
-            let coord1 = matrixToArray(fs.style.transform);
-            ropes[j].setAttribute('x2', +coord1[0] + +GridElSize.value / 2)
-            ropes[j].setAttribute('y2', +coord1[1] + maxh / 2)
-            let coord2 = matrixToArray(sc.style.transform);
-            ropes[j].setAttribute('x1', +coord2[0] + +GridElSize.value / 2)
-            ropes[j].setAttribute('y1', +coord2[1] + maxh / 2)
-        }
-        clearInterval(interval2)
-    }, 100);
-})
 window.onload = () => {
     grid.remove();
     loadScreen();
