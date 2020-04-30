b2eb5a519511d8ae219faf0c3b97dc95c368c60c
glyway <55627397+glyway@users.noreply.github.com>
2020-04-30 15:16:09 +0400

    RenderDesign

--- mainpage/renderImg.js
+++ mainpage/renderImg.js
@@ -13,7 +13,22 @@ const renderImg = () => {
     clone.style.height = maxHeight + 20 + "px";
     document.body.appendChild(clone);
     html2canvas(clone, {allowTaint: true}).then(canvas => {
-        document.body.appendChild(canvas)
+        document.querySelector(".render_result").appendChild(canvas)
+        canvas.style.width = "100%"
+        canvas.style.maxHeight ="calc(100% - 61px)"
+        canvas.id = "imgc"
+        //let img = canvas.toDataURL("image/png");
+        //document.getElementById("downloadIMG").href = img;
+        
     });
+    document.querySelector(".render_result").style.display = "block";
+    document.getElementById("closeRender").addEventListener('click', ()=>{
+        let imgc = document.getElementById('imgc');
+        document.querySelector(".render_result").removeChild(imgc);
+        document.querySelector(".render_result").style.display = "none";
+    })
+    document.getElementById("downloadIMG").addEventListener('click', ()=>{
+
+    })
     document.body.removeChild(clone);
 }
