cd236b7c40015c393ed84122f6081867ccf73cfb
Nikita <nbreskanu73@gmail.com>
2020-05-01 12:43:30 +0400

    improved render

--- mainpage/renderImg.js
+++ mainpage/renderImg.js
@@ -1,7 +1,6 @@
 const calculateHeight = (clone) => {
     let heights = Array.from(clone.querySelectorAll(".item")).map(elem => {
         let s = elem.style.transform;
-        console.log(elem.offsetHeight);
         return parseInt(s.split("translateY")[1].slice(1, -3)) + elem.offsetHeight;
     })
     let textHeights = Array.from(clone.querySelectorAll(".text_div")).map(div => {
@@ -23,35 +22,41 @@ const prepareText = (clone) => {
     })
 }
 
+const closeRender = () => {
+    let imgc = document.getElementById('imgc');
+    if (imgc)
+        document.querySelector(".render_result").removeChild(imgc);
+    document.querySelector(".black_screen").style.opacity = 0;
+    document.querySelector(".black_screen").style.pointerEvents = "none";
+}
+
 const renderImg = () => {
     let clone = document.querySelector(".wrapper").cloneNode(true);
     document.body.appendChild(clone);
     prepareText(clone);
     let maxHeight = calculateHeight(clone);
     clone.className = "wrapper-clone";
-    clone.style.height = maxHeight + 50 + "px";
+    clone.style.height = maxHeight + parseInt(TextGapY.value) + 50 + "px";
     clone.style.position = "relative";
     clone.style.left = 0 + "px";
     clone.style.top = 0 + "px";
+    clone.style.width = window.innerWidth - 300 + parseInt(TextGapX.value) + "px";
     html2canvas(clone, {allowTaint: true}).then(canvas => {
         document.querySelector(".render_result").appendChild(canvas)
         canvas.style.width = "100%"
         canvas.style.maxHeight ="calc(100% - 61px)"
         canvas.id = "imgc"
         document.querySelector(".render_result").style.height = maxHeight + 150 + 'px';
-        //let img = canvas.toDataURL("image/png");
-        //document.getElementById("downloadIMG").href = img;
+        document.querySelector(".black_screen").style.opacity = 1;
+        document.querySelector(".black_screen").style.pointerEvents = "";
+        document.querySelector(".black_screen").addEventListener("click", function blclick(e) {
+            if (e.target.className == "render_result" || e.target.id == "imgc") {
+                return;
+            }
+            closeRender();
+            document.querySelector(".black_screen").removeEventListener("click", blclick);
+        })
+        document.body.removeChild(clone);
         
     });
-    document.querySelector(".black_screen").style.opacity = 1;
-    document.querySelector(".black_screen").style.pointerEvents = "";
-    document.getElementById("closeRender").addEventListener('click', ()=>{
-        let imgc = document.getElementById('imgc');
-        if (imgc)
-            document.querySelector(".render_result").removeChild(imgc);
-        document.querySelector(".black_screen").style.opacity = 0;
-        document.querySelector(".black_screen").style.pointerEvents = "none";
-        
-    })
-    document.body.removeChild(clone);
 }
